78de0f0bd8f64b993fba095a0ec756340a33e341
Farid Adam <faridibin@gmail.com>
2021-05-06 22:12:04 -0400

    Create RespectSnakeCase.php

---
+++ src/Faridibin/LaravelJsonResponse/Traits/Http/Response/RespectSnakeCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Faridibin\LaravelJsonResponse\Traits\Http\Response;
+
+use Illuminate\Support\Str;
+
+trait RespectSnakeCase
+{
+    /**
+     * Sanitize response data to snake case.
+     *
+     * @param  \Illuminate\Support\Collection  $data
+     * @return mixed
+     */
+    public function handleResponseData($data)
+    {
+        $replaced = [];
+
+        foreach ($data as $key => $value) {
+            $snakeKey = \is_array($data) ? Str::snake($key) : $key;
+
+            $replaced[$snakeKey] = \is_array($value) ? $this->handleResponseData($value) : $value;
+        }
+
+        return $replaced;
+    }
+}
