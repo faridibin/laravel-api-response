58617362bf47f06c153f7e123070629873302ee2
Farid Adam <faridibin@gmail.com>
2022-06-12 02:33:56 -0400

    Update OutputApiJsonResponse.php

--- src/Middleware/OutputApiJsonResponse.php
+++ src/Middleware/OutputApiJsonResponse.php
@@ -3,32 +3,21 @@
 namespace Faridibin\LaravelApiJsonResponse\Middleware;
 
 use Closure;
-use Faridibin\LaravelApiJsonResponse\Traits\HasApiResponse;
+
 use Faridibin\LaravelApiJsonResponse\ApiResponse;
 use Illuminate\Http\Request;
 
 class OutputApiJsonResponse
 {
-    use HasApiResponse;
-
-    /**
-     * The recommended response to send to the client.
-     *
-     * @var \Illuminate\Http\Response|\Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
-     */
-    private $response;
-
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse|\Illuminate\Http\RedirectResponse|array
      */
     public function handle(Request $request, Closure $next)
     {
-        $this->response = new ApiResponse($request, $next);
-
-        return $this->makeJsonResponse();
+        return (new ApiResponse($request, $next))->makeJsonResponse();
     }
 }
