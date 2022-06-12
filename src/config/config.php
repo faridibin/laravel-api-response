ecdcb4d5ab192fbfb051d606489eda79d69f5a06
Farid Adam <faridibin@gmail.com>
2022-06-12 01:55:26 -0400

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -1,27 +1,48 @@
 <?php
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Faridibin\LaravelApiJsonResponse\Exceptions\ExceptionHandler;
 use Illuminate\Http\Response;
-use Illuminate\Validation\ValidationException;
-use Faridibin\LaravelJsonResponse\JsonResponse;
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | URI Cases
+    |--------------------------------------------------------------------------
+    |
+    | All API responses have resources. This defines just a way
+    | of naming the resources to resemble natural language while avoiding
+    | spaces, apostrophes, and other exotic characters.
+    |
+    | If you have multiple naming conventions in your tables or models
+    | you may need to configure this option to specify which one you
+    | as a default.
+    |
+    | Supported: "camel", "snake", "spinal"
+    |
+    */
+    'uri_case' => 'snake',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Handling Exceptions
+    |--------------------------------------------------------------------------
+    |
+    | By default, all exceptions are handled by the default exception handler.
+    | You may specify a different exception handler for each exception
+    | as required, but they're a perfect start for most applications.
+    |
+    */
     'exceptions' => [
-        /**
-         * Show model not found when receiving this error
-         */
-        ModelNotFoundException::class => [
-            'error' => 'Model not found',
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class => [
+            'setMessage' => 'No query results for model found!',
             'setStatusCode' => Response::HTTP_NOT_FOUND
         ],
 
-        /**
-         * Add all the errors from the validation and continue
-         */
-        ValidationException::class => function (ValidationException $e, JsonResponse $json) {
-            $json
+        \Illuminate\Validation\ValidationException::class => function (\Illuminate\Validation\ValidationException $e, ExceptionHandler $handler) {
+            $handler
                 ->mergeErrors($e->errors())
-                ->setStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+                ->setStatusCode($e->status ?? Response::HTTP_UNPROCESSABLE_ENTITY);
         }
     ]
 ];
