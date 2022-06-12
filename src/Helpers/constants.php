fe02e5b5d7f23a9a41ba25edac3068c4391dfeed
Farid Adam <faridibin@gmail.com>
2022-06-12 01:55:47 -0400

    Update constants.php

--- src/Helpers/constants.php
+++ src/Helpers/constants.php
@@ -2,3 +2,5 @@
 
 define('LARAVEL_API_JSON_RESPONSE_CONFIG', 'api-json-response');
 define('LARAVEL_API_JSON_RESPONSE_KEY', 'laravel-api-json-response');
+define('LARAVEL_API_JSON_RESPONSE_URI_CASES', ['camel', 'slug', 'snake', 'spinal']);
+define('LARAVEL_API_JSON_RESPONSE_URI_CASE', 'snake');
