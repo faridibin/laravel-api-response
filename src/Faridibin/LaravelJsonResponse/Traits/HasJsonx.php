dd303d7212ee31014d1cfe20ff7239f2e165c38e
Farid Adam <faridibin@gmail.com>
2022-06-20 03:22:52 -0400

    Create HasJsonx.php

---
+++ src/Faridibin/LaravelJsonResponse/Traits/HasJsonx.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Faridibin\LaravelApiResponse\Traits;
+
+use Faridibin\LaravelApiResponse\JsonResponse;
+
+trait HasJsonx
+{
+    /** @var  JsonResponse */
+    private $_json = null;
+
+    public function hasErrors()
+    {
+        return $this->json()->hasErrors();
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function json()
+    {
+        return $this->_json ?: ($this->_json = json_response());
+    }
+
+    public function responseArray()
+    {
+        return $this->json()->toArray();
+    }
+
+    public function getStatusCode()
+    {
+        return $this->json()->getStatusCode();
+    }
+
+    public function isSuccess()
+    {
+        return $this->json()->isSuccess();
+    }
+
+    public function hasToken()
+    {
+        return $this->json()->hasToken();
+    }
+}
