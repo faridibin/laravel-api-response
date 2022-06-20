79e25ec09d2a9b1d2f94803c80ee4a5701e67858
Farid Adam <faridibin@gmail.com>
2022-06-20 03:22:06 -0400

    Update helpers.php

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -2,20 +2,43 @@
 
 use Illuminate\Support\Str;
 
+/**
+ * ApiResponse represents an HTTP response in JSON format.
+ *
+ * Note that this class does not force the returned JSON content to be an
+ * object. It is however recommended that you do return an object as it
+ * protects yourself against XSSI and JSON-JavaScript Hijacking.
+ *
+ * @author Farid Adam <me@faridibin.tech>
+ */
+
+if (!function_exists('api_response')) {
+    /**
+     * Gets the apps ApiResponse
+     *
+     * @return \Faridibin\LaravelApiResponse\ApiResponse
+     */
+    function api_response()
+    {
+        return app(LARAVEL_API_RESPONSE_KEY);
+    }
+}
+
 if (!function_exists('format_with_uri_case')) {
     /**
      * Validates uri case against supported cases.
      *
      * @param  string  $case
+     *
      * @return string
      */
     function validate_uri_case(string $case)
     {
-        if (in_array($case, LARAVEL_API_JSON_RESPONSE_URI_CASES)) {
+        if (in_array($case, LARAVEL_API_RESPONSE_URI_CASES)) {
             return ($case === 'spinal') ? 'slug' : $case;
         }
 
-        return LARAVEL_API_JSON_RESPONSE_URI_CASE;
+        return LARAVEL_API_RESPONSE_URI_CASE;
     }
 
     /**
@@ -23,6 +46,7 @@ if (!function_exists('format_with_uri_case')) {
      *
      * @param  array  $data
      * @param  string  $case
+     *
      * @return mixed
      */
     function format_with_uri_case(array $data, string $case)
