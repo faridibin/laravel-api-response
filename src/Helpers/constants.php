4cb5ef89ea426c68764e7cf4c7918ce6d2c723e6
Farid Adam <faridibin@gmail.com>
2022-06-20 03:22:08 -0400

    Update constants.php

--- src/Helpers/constants.php
+++ src/Helpers/constants.php
@@ -1,6 +1,17 @@
 <?php
 
-define('LARAVEL_API_JSON_RESPONSE_CONFIG', 'api-json-response');
-define('LARAVEL_API_JSON_RESPONSE_KEY', 'laravel-api-json-response');
-define('LARAVEL_API_JSON_RESPONSE_URI_CASES', ['camel', 'slug', 'snake', 'spinal']);
-define('LARAVEL_API_JSON_RESPONSE_URI_CASE', 'snake');
+/**
+ * ApiResponse represents an HTTP response in JSON format.
+ *
+ * Note that this class does not force the returned JSON content to be an
+ * object. It is however recommended that you do return an object as it
+ * protects yourself against XSSI and JSON-JavaScript Hijacking.
+ *
+ * @author Farid Adam <me@faridibin.tech>
+ */
+
+define('LARAVEL_API_RESPONSE_CONFIG', 'api-response');
+define('LARAVEL_API_RESPONSE_KEY', 'laravel-api-response');
+define('LARAVEL_API_RESPONSE_AUTH_HEADER', 'Authorization');
+define('LARAVEL_API_RESPONSE_URI_CASES', ['camel', 'slug', 'snake', 'spinal']);
+define('LARAVEL_API_RESPONSE_URI_CASE', 'snake');
