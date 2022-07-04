93024ae91c09be039d169b8f48b3827c5437d25f
Farid Adam <faridibin@gmail.com>
2022-07-04 00:24:55 -0400

    Create XmlResponse.php

---
+++ src/Http/XmlResponse.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Faridibin\LaravelApiResponse\Http;
+
+use Illuminate\Http\ResponseTrait;
+use Illuminate\Support\Traits\Macroable;
+use Symfony\Component\HttpFoundation\Response as BaseResponse;
+
+class XmlResponse extends BaseResponse
+{
+    use Macroable, ResponseTrait {
+        Macroable::__call as macroCall;
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param  mixed  $data
+     * @param  int  $status
+     * @param  array  $headers
+     * @param  int  $options
+     *
+     * @return void
+     */
+    public function __construct($data = null, $status = 200, $headers = [], $options = 0)
+    {
+        dd($data, $status, $headers, $options);
+    }
+}
