a406b5f1490bc574d0d5ece67512455a0052d695
Farid Adam <faridibin@gmail.com>
2022-07-24 15:09:59 -0400

    Create InitialTest.php

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Faridibin\LaravelApiResponse\Tests\Feature;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class InitialTest extends BaseTestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
